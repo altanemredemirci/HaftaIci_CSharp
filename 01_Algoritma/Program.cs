3cfdfe4f46506410dea8f9e9378da8a2ccb018e1
101-Hoca-Pc <101-Hoca-Pc@DESKTOP-58CMK8T>
2024-08-15 19:15:40 +0300

    Add project files.

---
+++ 01_Algoritma/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _01_Algoritma
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            /*
+               
+             YAZILIM :Var olan bir problemi çözmek veya çözümü geliştirmek için yapılır.
+             1-Backend => UI alanında gelen istekleri derleyen" yapıdır.
+             2-Frontend => User Interface(UI/UX) => Html,Css,JavaScript
+
+                 Mobile App => 
+                 Game
+                 Desktop App
+                 Web App
+
+
+            Yazılım uzmanlığı => .Net Developer 
+
+            Framework: Yazılımcılar tarafından geliştirilen ve yazılım geliştiricilerin kullanımına açılan hazır kod kütüphanesidir.
+
+            .Net Framework: C# tabanlı farklı platformlarda kodlanmış projelerin ortak bir tabanda geliştirilmesini ve çalıştırılmasını sağlar.
+                
+                *Games
+                *Web
+                *Console
+                *Desktop
+                *Machine Learning
+
+
+            */
+
+            Console.WriteLine("İbrahim");
+
+        }
+    }
+}

9ab4c5b6d9510c19430539f3f3fb04e8367c0e00
Altan Emre <altan_emre_1989@hotmail.com>
2024-08-20 20:50:13 +0300

    Variables Completed

--- 02_Degiskenler_VeriTipleri/Program.cs
+++ 02_Degiskenler_VeriTipleri/Program.cs
@@ -37,11 +37,33 @@
                  *Tamsayılar-Integer
                  *Ondalıklı Sayılar-Float
                  *Decimal:
+                      //float bakiye = 50000.500f;
+                      //double bakiye = 50000.500;
+                      
+                      //decimal sayi = 10012837283712;
+                      //decimal sayi2 = 100.0m;
+                 
                  
             SÖZEL TİPLER(STRING)
-            MANTIKSAL TİP(BOOLEAN)
+                *char : Tek karakter alır ve '' tek tırnak ile tanımlanır.
+                                //char harf = '1';
+                    //char harf1 = 'A';
+                    //char harf2 = '!';
 
+                    //Console.WriteLine(harf);
+                    //Console.WriteLine(harf1);
+                    //Console.WriteLine(harf2);
+
+                *string : Sözel değerleri tutar. Ram bellek boyutu kadar karakter alabilir ve "" çift tırnak ile tanımlanır.
+                     string ad = "Altan Emre"; 
+                     Console.WriteLine(ad);
+
+            MANTIKSAL TİP(BOOLEAN)
+                 *bool: Sadece true ve false değerlerinden birini alabilen veri tipidir.
+                    //bool sonuc = 5 > 3;
+                    //Console.WriteLine(sonuc);
              */
+
 
             //int yas = 34;
             //long tc = 12345678909;
@@ -49,21 +71,85 @@
             //int sayi = 12322322;
 
 
-            string ad = "Altan Emre";
-            Console.WriteLine(ad);
-
-
-
             // İki Kardeşin Yaş toplamı
-            int yas1 = 33;
-            int yas2 = 34;
+            //int yas1 = 33;
+            //int yas2 = 34;
 
-            Console.WriteLine(yas1+yas2);
+            //Console.WriteLine(yas1+yas2);
 
 
-            //Çoklu Değişken atama
-            //Console.Write Çeşitleri
-            //int float toplamı
+            //Tanımlı değişken değeri revize edildi.
+            //int sayi = 5;
+            //sayi = 6;
+            //sayi = 4;
+
+
+            // ***** CONSOLE SINIFI İŞLEMLERİ *****
+
+            //Console.WriteLine("İçine yazılan her şey Konsol ekranına yazılır.");
+            //Console.WriteLine("Her WriteLine metodu bir satır boyunca yer kaplar.");
+
+
+            //Console.Write("Değer yazıldıktan sonra imleç o satırda kalır.");
+            //Console.Write("Değer yazıldıktan sonra imleç o satırda kalır.");
+            //Console.Write("Değer yazıldıktan sonra imleç o satırda kalır.");
+            //Console.Write("Değer yazıldıktan sonra imleç o satırda kalır.");
+
+
+            //int s1 = 12;
+            //int s2 = 13;
+            //float f1 = 12.5f;
+            //double d1 = 150.450;
+            //string ad = "Altan";
+            //string no = "154";
+
+            //Console.WriteLine(s1+s2); //25
+            //Console.WriteLine(s1+f1); //24.5
+            //Console.WriteLine(s1+d1); //162.45
+            //Console.WriteLine(f1+d1); //162.95
+
+            //Console.WriteLine(ad+no); //Altan154
+            //Console.WriteLine(no+s1); //15412
+
+
+
+            //int s1 = 12;
+            //double d1= 5.4;
+
+            //double sonuc = s1 + d1;
+
+
+
+
+            //int s1 = 12;
+            //double d1 = 58347894739847;
+
+            //int sonuc = (int)(s1 + d1);
+            //Console.WriteLine(sonuc);
+
+
+
+            //int s1 = 12;
+            //int s2 = 13;
+            //int toplam = s1 + s2;
+            ////Console.WriteLine("Toplam:"+toplam);
+            ////Console.WriteLine("Toplam:"+(s1+s2)); //Toplam:25
+            //Console.WriteLine("Sayı1:"+s1+" Sayı2:"+s2+" Toplam:"+toplam);
+            //Console.WriteLine("Sayı1:{0} Sayı2:{1} Toplam:{2}",s1,s2,toplam);
+            //Console.WriteLine($"Sayı1:{s1} Sayı2:{s2} Toplam:{toplam}");
+
+
+
+            //CONSTANT => sabit değişken anlamına gelir ve değer tanımlandıktan sonra değiştirilemez.
+
+            //const int sayi = 5;
+
+            //sayi = 6;
+
+            //Console.WriteLine(sayi);
+
+
+
 
 
         }

a88b19e950c66de56f60397f7e0cb26e5bb46ff5
Altan Emre <altan_emre_1989@hotmail.com>
2024-08-20 19:09:34 +0300

    Algoritma Bitti

--- 01_Algoritma/Program.cs
+++ 01_Algoritma/Program.cs
@@ -29,9 +29,151 @@
                 *Machine Learning
 
 
-            */
 
-            Console.WriteLine("İbrahim");
+            ## ******** ALGORİTMA ********* ##
+            Algoritma, Var olan bir problemi veya geliştirme ihtiyacını çözmek için yapılacak çözüm adımlarının mantıklı sıralamasıdır.
+
+            1-Başlangıç ve Bitiş belli olmalı
+            2-Sonluluk (karşılaşılacak bütün olasılıklar bir sonuca varmalı)
+            3-Kesinlik (çözüm adımları net anlaşılır ve açık olmalıdır.)
+            4-Etkinlik (Modüler ve Genellenebilir kodlama)
+            5-Verimilik (Performans,Zaman,Maliyet)
+
+
+
+            ÇAY DEMLEME ALGORİTMASI
+            1-Başla
+            2-Mutfağa Git
+            3-Çaydanlığa Su Koy
+            4-Ocağı Yak
+            5-Su Kaynadı Mı? T->7  F->6
+            6-Bekle ->5
+            7-Demliğe Çay Koy
+            8-Çaydanlığa Su Koy
+            9-Çay Demlendi Mi? T->11 F->10
+            10-Bekle ->9
+            11-Servis Et
+            12-Bitti
+
+
+            ** Su Yok
+            ** Çay Yok
+
+
+            1-Başla
+            2-Mutfağa Git
+            3-Su ve Çay Var Mı? T->9 F->4
+            4-Bakkala Git
+            5-Bakkal Açık Mı? T->7 F->6
+            6-Başka Bakkala Git ->5
+            7-Eksikleri Al
+            8-Eve Dön ->2 
+            9-Çaydanlığa Su Koy
+            10-Ocağı Yak
+            11-Su Kaynadı Mı? T->13  F->12
+            12-Bekle ->11
+            13-Demliğe Çay Koy
+            14-Çaydanlığa Su Koy
+            15-Çay Demlendi Mi? T->17 F->16
+            16-Bekle ->15
+            17-Servis Et
+            18-Bitti
+
+
+
+            ### EVDEN ÜÇÜNCÜ BİNYIL AKADEMİYE GİTME PROGRAMI ###
+            1-Başla
+            2-Daireden Çık
+            3-Asansör Mü Merdiven Mi? A->4 M->11
+            4-Asansör Katta Mı? T->6 F->5
+            5-Asansörü Çağır ->4
+            6-Asansöre Bin
+            7-İstenilen Kat tuşuna Bas
+            8-İstenilen Katta Mıyız? T->10 F->9
+            9-Bekle ->8
+            10-Asansörden İn ->13
+            11-1 Kat İn
+            12-İstenilen Katta Mıyız? F->11 T->13
+            13-Binadan Çık
+
+
+            14-Akbil Dolu Mu? T->17 F->15
+            15-Dolum İstasyonuna Git
+            16-Akbile Bakiye Yükle
+            17-Durağa Git
+            18-Hangi Toplu Taşımaya Binilecek? 14B
+            19-Toplu Taşıma Geldi Mi? F->20 T->21
+            20-Bekle ->19
+            21-Toplu Taşımaya Bin
+            22-Akbil Bas
+            23-Boş Yer Var Mı? T->25 F->24
+            24-Ayakta Git ->26
+            25-Otur
+            26-İstenile Durağına Geldik Mi? T->28 F->27
+            27-Bekle ->26
+            28-Toplu Taşımadan İn
+            29-Akademiye Yürü
+            30-Bitti
+            
+             
+             
+             ## EVDEN ÇÖP ATMA PROGRAMI
+            1-Başla
+            2-Daireden Çık
+            3-Asansör Mü Merdiven Mi? A->4 M->11
+            4-Asansör Katta Mı? T->6 F->5
+            5-Asansörü Çağır ->4
+            6-Asansöre Bin
+            7-0 tuşuna Bas
+            8-0. Katta Mıyız? T->10 F->9
+            9-Bekle ->8
+            10-Asansörden İn ->13
+            11-1 Kat İn
+            12-0.katta Mıyız? F->11 T->13
+            13-Binadan Çık
+            14-Kontaynere Git
+            15-Çöp At
+            16-Bitti
+
+
+
+            ## MİSAFİR YOLCU ETME PROGRAMI
+            1-Başla
+            2-Daireden Çık
+            3-Asansör Mü Merdiven Mi? A->4 M->11
+            4-Asansör Katta Mı? T->6 F->5
+            5-Asansörü Çağır ->4
+            6-Asansöre Bin
+            7-0 tuşuna Bas
+            8-0. Katta Mıyız? T->10 F->9
+            9-Bekle ->8
+            10-Asansörden İn ->13
+            11-1 Kat İn
+            12-0.katta Mıyız? F->11 T->13
+            13-Binadan Çık
+            14-Güle Güle De
+            15-Bitti
+
+            ## MİSAFİR KARŞILAMA PROGRAMI
+            1-Başla
+            2-Daireden Çık
+            3-Asansör Mü Merdiven Mi? A->4 M->11
+            4-Asansör Katta Mı? T->6 F->5
+            5-Asansörü Çağır ->4
+            6-Asansöre Bin
+            7-0 tuşuna Bas
+            8-0. Katta Mıyız? T->10 F->9
+            9-Bekle ->8
+            10-Asansörden İn ->13
+            11-1 Kat İn
+            12-0.katta Mıyız? F->11 T->13
+            13-Binadan Çık
+            14-Hoşgeldin de 
+            15-Bitti
+             
+             */
+
+           
 
         }
     }
---
+++ 02_Degiskenler_VeriTipleri/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace _02_Degiskenler_VeriTipleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ********* ### DEĞİŞKENLER - VARIABLES ### ***********
+
+            /*
+             Değişkenler, progralama içerisinde veri tanımlamak, veri taşımak ve veriyi işleme tabi, tutmak için kullanılan yazılım birimleridir.Her değişken tek data tutabilir. Değişken isimlendirilirken amacına uygun isim almalıdır.
+            
+
+
+            Değişken İsimlendirme Kuralları(Case-Sensitive)
+            yanlışlar: 1sayi, ?sayi, sayi?, ad soyad, ad!soyad
+            doğrular:  sayi1, _sayi, sayi_, adsoyad, ad_soyad
+
+            ** Bütün yazılım dillerinde geçerli tek özel karakter alt tire _ dir.
+
+
+            DEĞİŞKEN TANIMLAMA
+            veritipi degiskenadi = deger;
+
+
+            sayi1=34
+            sayi2=12
+             
+
+            a=12
+            b=45
+
+
+
+             // ********* ### VERİ TİPLERİ - DATATYPES ### ***********
+
+            SAYISAL TİPLER()
+                 *Tamsayılar-Integer
+                 *Ondalıklı Sayılar-Float
+                 *Decimal:
+                 
+            SÖZEL TİPLER(STRING)
+            MANTIKSAL TİP(BOOLEAN)
+
+             */
+
+            //int yas = 34;
+            //long tc = 12345678909;
+
+            //int sayi = 12322322;
+
+
+            string ad = "Altan Emre";
+            Console.WriteLine(ad);
+
+
+
+            // İki Kardeşin Yaş toplamı
+            int yas1 = 33;
+            int yas2 = 34;
+
+            Console.WriteLine(yas1+yas2);
+
+
+            //Çoklu Değişken atama
+            //Console.Write Çeşitleri
+            //int float toplamı
+
+
+        }
+    }
+}

b56010038b7ac6d1c1531554b09fdd4e73520cb2
Altan Emre <altan_emre_1989@hotmail.com>
2024-08-20 21:43:52 +0300

    Operatorler Continue

---
+++ 03_Operatorler_VeriTipiDonusumleri/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace _03_Operatorler_VeriTipiDonusumleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region ATAMA OPERATÖRÜ(=)
+            /*
+            int sayi = 5;
+
+
+            //İşlemli Atama
+
+            sayi += 1;  //sayi = sayi + 1;
+            sayi -= 1;  //sayi = sayi - 1;
+            sayi *= 1;  //sayi = sayi * 1;
+            sayi /= 1;  //sayi = sayi / 1;
+
+            Console.WriteLine(sayi);
+
+            */
+            #endregion
+
+            #region ARTTIRMA-AZALTMA OPERATÖRÜ(++ , --)
+            //1 arttırma veya 1 azaltma yapılabilir.
+            //int sayi = 5;
+
+            //sayi++;
+            //Console.WriteLine(sayi);
+
+            //sayi--;
+            //Console.WriteLine(sayi);
+
+            #endregion
+
+            #region ARİTMATİK OPERATÖRLER(+,-,*,/,%)
+
+            /*
+             int sayi = 1;
+             int sayi2 = 3;
+
+             int sonuc1 = sayi + sayi2;
+             int sonuc2 = sayi - sayi2;
+             int sonuc3 = sayi * sayi2;
+             int sonuc4 = sayi / sayi2;
+
+
+             int modAlma = sayi % sayi2;
+
+             Console.WriteLine(modAlma);
+             
+
+            int sayi = 10;
+            double bolen = 2;
+
+            //Console.WriteLine(sayi/bolen);
+
+
+            //int sonuc = (int)(sayi/bolen);
+            //double sonuc = sayi/bolen;
+            //Console.WriteLine(sonuc);
+
+            double sonuc2 = sayi / bolen;
+            Console.WriteLine(sonuc2);
+            */
+
+
+            #endregion
+
+            #region KIYAS OPERATÖRÜ (? :)
+
+            // Karşılaştırma sonucu TRUE ise ?, FALSE ise : tarafı atanır.
+
+            //int sayi = 7;
+
+            //string sonuc = sayi < 5 ? "Küçük" : "Büyük";
+
+            //int sonuc2 = sayi < 5 ? 0 : 1;
+
+            //bool sonuc3 = sayi < 5 ? true : false;
+
+
+            //Console.WriteLine(sonuc);
+
+
+            #endregion
+
+            #region KARŞILAŞTIRMA OPERATÖRLERİ
+
+            /*
+            >
+            <
+            >=
+            <=
+            ==  eşit mi?
+            ! = eşit değil mi?
+             */
+
+
+            #endregion
+
+
+            //MANTIKSAL OPERATÖRLER(     )
+            //AND Operatörü (&&)
+            //OR Operatörü (||)
+
+
+            //ERİŞİM OPERATÖRÜ
+
+            Console.ReadLine().Replace()
+
+        }
+    }
+}

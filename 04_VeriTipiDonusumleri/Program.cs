431499b708e09ab063a827082febbc096cbd3663
Altan Emre <altan_emre_1989@hotmail.com>
2024-08-22 21:43:43 +0300

    Karar Yapıları Devam..

---
+++ 04_VeriTipiDonusumleri/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _04_VeriTipiDonusumleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VERİTİPLERİ DONUSUMLERİ - CONVERSION
+
+
+
+            #region BİLİNÇSİZ TÜR DÖNÜŞÜMLERİ
+
+            //int sayi = 5;
+            //double sayi2 = sayi;
+
+
+            //BOXING
+            //double sayi = 757656757657576576;
+            //int sayi2 = (int)sayi;
+
+            //Console.WriteLine(sayi2);
+
+            #endregion
+
+            #region BİLİNÇLİ TÜR DÖNÜŞÜMLERİ
+
+            //Console.WriteLine("Yaş:");
+            //string yas = Console.ReadLine();
+            //int age = Convert.ToInt32(yas);
+
+
+            //Console.WriteLine("Yaş:");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(age+5);
+
+            //Console.WriteLine("Havale Miktarı:");
+            //double balance = Convert.ToDouble(Console.ReadLine());
+
+
+            //II. Yöntem
+
+            //Console.WriteLine("Yaş:");
+            //double age = double.Parse(Console.ReadLine());
+
+
+
+
+            #region İki kardeşin yaşlarını kullanıcıdan isteyiniz ve alınan yaşların ortalamasını ekrana yazdırınız.
+
+            //Console.WriteLine("1. Kardeş Yaşı:");
+            //int yas1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("2. Kardeş Yaşı:");
+            //int yas2 = Convert.ToInt32(Console.ReadLine());
+
+            //double ort = (yas1 + yas2) / 2.0;
+
+            //Console.WriteLine("Yaş Ortalaması:"+ort);
+            #endregion
+
+
+            // SAYISAL DEĞERİN SÖZEL DEĞERE DÖNÜŞTÜRÜLMESİ
+
+            //int sayi = 5;
+            //string metin = sayi.ToString();
+
+            #endregion
+
+        }
+    }
+}
---
+++ 05_If_Elseif_Else/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace _05_If_Elseif_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region IF ELSEIF ELSE
+
+            /*
+            ** Kodun akışı belirli şartlara göre yönlendirilecek ise CONDITION-KoşulYapıları kullanılır.
+
+            *If Elseif Else
+            *Switch Case
+
+            Bir sorguda ilk koşul if ile kontrol edilir.Alternatif koşullar else if deyimleri ile kontrol edilir ve bütün durumlar-koşullar kontrol edildiktan sonra geriye kalan koşullar için else deyimi kullanılır.
+            Bir kara yapısında if ve else bir defa tanımlanır ve else geriye kalan bütün durumlar olduğu için koşul ALMAZ.
+
+
+            
+            1-Başla
+            2-sayi=Bir sayı giriniz
+            3-Eğer sayi>0 ise Ekrana "Pozitif" yaz
+            4-Değilse Eğer sayi<0 ise Ekrana "Negatif" yaz
+            5-Değilse Ekrana "Sayı 0'a eşittir" yaz
+            6-Bitti
+
+              Console.WriteLine("Bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi > 0)
+            {
+                Console.WriteLine("Pozitif");
+            }
+            else if (sayi < 0)
+            {
+                Console.WriteLine("Negatif");
+            }
+            else
+            {
+                Console.WriteLine("Sayı 0'a eşittir");
+            }
+
+
+
+
+
+            */
+
+            #region Kullanıcıdan haftanın gün sayısını alınız.Girilen rakama göre gün adını ekrana yazdırınız.
+
+            Console.WriteLine("Haftanın kaçıncı günündeyiz?");
+            int gun = Convert.ToInt32(Console.ReadLine());
+
+
+            if (gun == 1)
+            {
+                Console.WriteLine("Pazartesi");
+            }
+            else if (gun == 2)
+            {
+                Console.WriteLine("Salı");
+            }
+            else if (gun == 3)
+            {
+                Console.WriteLine("Çarşamba");
+            }
+            else if (gun == 4)
+            {
+                Console.WriteLine("Perşembe");
+            }
+            else if (gun == 5)
+            {
+                Console.WriteLine("Cuma");
+            }
+            else if (gun == 6)
+            {
+                Console.WriteLine("Cumartesi");
+            }
+            else if (gun == 7)
+            {
+                Console.WriteLine("Pazar");
+            }
+            else
+            {
+                Console.WriteLine("Hafta 7 günden oluşur!!!");
+            }
+            #endregion
+
+
+
+
+            #endregion
+        }
+    }
+}
